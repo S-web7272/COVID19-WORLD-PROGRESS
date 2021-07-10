e41c3c539a92cdea4680513d23d69cbee897d628
smita srivastava <tanusrivastava7272@gmail.com>
2021-07-10 15:34:47 +0530

    added a graph

--- app.py
+++ app.py
@@ -6,17 +6,40 @@ import streamlit as st
 
 
 def load_data():
-    return pd.read_csv('data/country_vaccinations.csv',parse_dates=['date'],dayfirst=True, index_col='date')
+    df =  pd.read_csv('data/country_vaccinations.csv',
+                        parse_dates=['date'],
+                        dayfirst=True,
+                        index_col='date',
+                        )
+    df.drop(['iso_code'],axis=1,inplace=True)
+    return df
 
 st.set_page_config(layout="wide")
 df = load_data()
 
+if st.sidebar.checkbox('View raw data'):
+    st.write(df.head())
+    st.write(df.shape)
+    st.write(df.describe())
+
+
 st.title("covid-19 world vaccination progress visualisation")
 
-st.sidebar.subheader("change over time (Univariate analysis)")
-cols = ['country','iso_code','total_vaccinations','people_vaccinated','people_fully_vaccinated','daily_vaccinations_raw','daily_vaccinations','total_vaccinations_per_hundred','people_vaccinated_per_hundred','people_fully_vaccinated_per_hundred','daily_vaccinations_per_million','vaccines','source_name','source_website']
+st.sidebar.subheader("Columns in dataframe")
+for col in df:
+    st.sidebar.write(col, df[col].dtype)
 
-col = st.sidebar.selectbox("select a column",cols)
+st.header("Univariate analysis of numrical data")
 
-fig = px.line(df, x=df.index, y=col,)
-st.plotly_chart(fig,use_container_width=True)
+cols = ['total_vaccinations','people_vaccinated']
+c = st.beta_columns(2)
+col = c[0].selectbox("select a column",cols)
+gap = c[1].selectbox("select a range to display",['D','3D','W','2W','M','2M'])
+sub_df = df[col].resample(gap).mean()
+c = st.beta_columns(2)
+c[0].write(sub_df)
+fig =  px.bar(sub_df,x=sub_df.index,y=col)
+c[1].plotly_chart(fig)
+
+
+
